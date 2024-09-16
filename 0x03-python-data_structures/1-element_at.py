de72f5b3452f5bd5bb47c0d7baf55b2b4c4b3d68
nati-sahlu <natansahlu5@gmail.com>
2024-09-16 12:35:56 -0700

    task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-# Function that replaces an element in a list at a specific position
 
-def replace_in_list(my_list, idx, element):
-    if idx >= 0 and idx < len(my_list):
-        my_list[idx] = element
-    return my_list
+def element_at(my_list, idx):
+    """Retrieve an element from a list at a specific index."""
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
+
