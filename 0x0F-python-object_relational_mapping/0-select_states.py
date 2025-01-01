696a2ac3f09fe972fd85f8e79df4deecd54712c2
nati-sahlu <natansahlu5@gmai.com>
2025-01-02 01:07:24 +0300

    Add script to list states from MySQL

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+def list_states():
+    """Lists all states in the hbtn_0e_0_usa database."""
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=mysql_username, passwd=mysql_password,
+                         db=database_name)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    list_states()
+
