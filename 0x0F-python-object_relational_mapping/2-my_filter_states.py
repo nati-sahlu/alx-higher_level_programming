24028ab335402a967e8fb75c2dab6ccbdb4cbb70
nati-sahlu <natansahlu5@gmai.com>
2025-01-07 05:39:38 +0300

    task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-
-import MySQLdb
-from sys import argv
-
 '''
 Script that lists all states from the database
 '''
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
     cont = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-
-import MySQLdb
-from sys import argv
-
 '''
 script that lists all states from the database
 '''
+import MySQLdb
+from sys import argv
+
+
 if __name__ == "__main__":
     cont = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
