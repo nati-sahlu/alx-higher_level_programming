91a668fda06ae925b8ebf727874881ed10c2e049
nati-sahlu <natansahlu5@gmai.com>
2025-01-07 05:45:41 +0300

    task

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -2,14 +2,7 @@
 """
 class definition of a State and an instance Base = declarative_base()
 """
-
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from relationship_state import Base
-
-
-class City(Base):
-    """
+"""
     City class:
     inherits from Base
     links to the MySQL table states
@@ -20,6 +13,13 @@ class City(Base):
     class attribute state_id that represents a column of an integer,
     can’t be null and is a foreign key to states.id
     """
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_state import Base
+
+
+class City(Base):
+
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -2,16 +2,7 @@
 """
 class definition of a State and an instance Base = declarative_base()
 """
-
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-
-Base = declarative_base()
-
-
-class State(Base):
-    """
+"""
     State class:
     inherits from Base
     links to the MySQL table states
@@ -20,6 +11,14 @@ class State(Base):
     class attribute name that represents a column of a string
     with maximum 128 characters and cant be null
     """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
