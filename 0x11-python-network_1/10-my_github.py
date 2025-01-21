9e6a89adf53864659c50402387d88390c7d5a570
nati-sahlu <natansahlu5@gmai.com>
2025-01-21 20:53:25 +0300

    pyreq

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """
-Python script that takes your Github credentials (username and password)
-and uses the Github API to display your id
+Python script that takes your Github credentials (username and password) and uses the Github API to display your id
 """
 import requests
-from requests.auth import HTTPBasicAuth
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    url = 'https://api.github.com/users/{}'.format(argv[1])
-    r = requests.get(url,
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, token))
+    
+    if response.status_code == 200:
+        print(response.json().get("id"))
+    else:
+        print("None")
+
