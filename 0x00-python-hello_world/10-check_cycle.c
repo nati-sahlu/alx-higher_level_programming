50dc8e5c5d72d7fed1c491432955fa997f919e91
nati-sahlu <natansahlu5@gmail.com>
2025-06-19 15:02:28 -0700

    better solution

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,32 +1,26 @@
 #include "lists.h"
 
 /**
- * check_cycle - identifies if a linked list contains a cycle
- * @list: pointer to the head node of the linked list
- * 
- * Description: Uses Floyd's Tortoise and Hare algorithm to
- * efficiently detect cycles without extra memory usage
- * used slow and fast as a variable
- * 
- * Return: 1 if cycle exists, 0 otherwise
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-int check_cycle(listint_t *list);
+int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
-	
-	if (!list)
-		return (0);
-	
-	while (slow && fast && fast->next)
-	{
-		
-		slow = slow->next;
-		fast = fast->next->next;
+        listint_t *slow = list;
+        listint_t *fast = list;
 
-		if (slow == fast)
-			return (1);
-	}
-	
-	return (0);
+        if (!list)
+                return (0);
+
+        while (slow && fast && fast->next)
+        {
+                slow = slow->next;
+                fast = fast->next->next;
+                if (slow == fast)
+                        return (1);
+        }
+
+        return (0);
 }
