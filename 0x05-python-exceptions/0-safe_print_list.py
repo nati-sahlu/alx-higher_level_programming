5e22f2037426001a508cba6d0db77324b4afebec
nati-sahlu <natansahlu5@gmail.com>
2025-06-20 06:49:57 -0700

    fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
@@ -10,4 +11,3 @@ def safe_print_list(my_list=[], x=0):
         pass
     print()  # for the newline
     return count
-
