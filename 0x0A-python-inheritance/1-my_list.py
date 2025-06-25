c9e306166ace1c53b2641b6065a9b163c38f48f4
nati-sahlu <natansahlu5@gmail.com>
2025-06-25 05:10:24 +0300

    task

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,11 +5,10 @@
 
 class MyList(list):
     """Custom list that inherits from built-in list."""
-    
 
     def print_sorted(self):
         """Print a list in sorted ascending order."""
         try:
             print(sorted(self))
         except TypeError as e:
-            raise TypeError("All elements must be of the same comparable type") from e
+            raise TypeError("elements must be of the same  type") from e
