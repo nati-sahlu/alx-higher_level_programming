3b30e0da9ace656ea6f53e43574e915495a63114
nati-sahlu <natansahlu5@gmail.com>
2025-06-26 00:51:55 +0300

    task6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,5 +80,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns string info about this rectangle."""
-        return f"[{type(self).__name__}] ({self.id}) \
-                {self.x}/{self.y} - {self.width}/{self.height}"
+        return (f"[{type(self).__name__}] ({self.id})"
+                f"{self.x}/{self.y} - {self.width}/{self.height}")
